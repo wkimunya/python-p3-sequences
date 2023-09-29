18ecc55be49d855a1854dce3e1fb9735f8fe9108
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-09-29 09:49:28 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+
+    if length <= 0:
+        print(fibonacci_list) 
+        return
+    elif length >= 1:
+        fibonacci_list.append(0)  
+    if length >= 2:
+        fibonacci_list.append(1)  
+
+    a, b = 0, 1
+
+    for _ in range(2, length):
+        a, b = b, a + b  
+        fibonacci_list.append(b)
+
+    # Print the list
+    print(fibonacci_list)
+
